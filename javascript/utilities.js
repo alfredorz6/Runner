92c6b1ae792f92d6f0023a6a547a664f4f4bf4af
Alfredo Ramirez Mendez <alfredo.rz6@gmail.com>
2021-06-26 19:07:32 -0700

    sound effect and gameover screen

--- javascript/utilities.js
+++ javascript/utilities.js
@@ -77,7 +77,7 @@ function resetGame(){
 }
 
 function resetPlayer(){
-    if (lives === 0) {
+    if (lives < 1) {
         gameOver()
     } else if (status === 'burned'){
         runner.x = canvas.width/2 - runner.width/2
